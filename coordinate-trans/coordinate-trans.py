2a43f986b38671381ab4601921ff30bd037cf46e
Liu siyu <137892154+infinitelarge@users.noreply.github.com>
2023-07-28 10:56:06 +0800

    Update coordinate-trans.py

--- coordinate-trans/coordinate-trans.py
+++ coordinate-trans/coordinate-trans.py
@@ -59,3 +59,137 @@ def ecliptic_to_equatorial(lon, lat, epoch):
     ra = np.degrees(ra)
     dec = np.degrees(dec)
     return ra, dec
+#计算本地地平坐标
+def equatorial_to_horizontal(ra, dec, lat, lon, jd, lst):
+    """
+    Convert equatorial coordinates to horizontal coordinates.
+
+    Parameters
+    ----------
+    ra : float
+        Right ascension in degrees.
+    dec : float
+        Declination in degrees.
+    lat : float
+        Latitude of observer in degrees.
+    lon : float
+        Longitude of observer in degrees.
+    jd : float
+        Julian date.
+    lst : float
+        Local sidereal time in degrees.
+
+    Returns
+    -------
+    az : float
+        Azimuth in degrees.
+    alt : float
+        Altitude in degrees.
+    """
+    ra = np.radians(ra)
+    dec = np.radians(dec)
+    lat = np.radians(lat)
+    lon = np.radians(lon)
+    lst = np.radians(lst)
+    ha = lst - ra
+    alt = np.arcsin(np.sin(dec) * np.sin(lat) + np.cos(dec) * np.cos(lat) * np.cos(ha))
+    az = np.arctan2(np.sin(ha), np.cos(ha) * np.sin(lat) - np.tan(dec) * np.cos(lat))
+    az = np.degrees(az)
+    alt = np.degrees(alt)
+    return az, alt
+#地平坐标转到赤道坐标
+def horizontal_to_equatorial(az, alt, lat, lon, jd, lst):
+    """
+    Convert horizontal coordinates to equatorial coordinates.
+
+    Parameters
+    ----------
+    az : float
+        Azimuth in degrees.
+    alt : float
+        Altitude in degrees.
+    lat : float
+        Latitude of observer in degrees.
+    lon : float
+        Longitude of observer in degrees.
+    jd : float
+        Julian date.
+    lst : float
+        Local sidereal time in degrees.
+
+    Returns
+    -------
+    ra : float
+        Right ascension in degrees.
+    dec : float
+        Declination in degrees.
+    """
+    az = np.radians(az)
+    alt = np.radians(alt)
+    lat = np.radians(lat)
+    lon = np.radians(lon)
+    lst = np.radians(lst)
+    ha = np.arctan2(np.sin(az), np.cos(az) * np.sin(lat) + np.tan(alt) * np.cos(lat))
+    dec = np.arcsin(np.sin(lat) * np.sin(alt) - np.cos(lat) * np.cos(alt) * np.cos(az))
+    ra = lst - ha
+    ra = np.degrees(ra)
+    dec = np.degrees(dec)
+    return ra, dec
+#从 B1950.0 标准分点赤道坐标转到银道坐标
+def equatorial_to_galactic(ra, dec):
+    """
+    Convert equatorial coordinates to galactic coordinates.
+
+    Parameters
+    ----------
+    ra : float
+        Right ascension in degrees.
+    dec : float
+        Declination in degrees.
+
+    Returns
+    -------
+    lon : float
+        Galactic longitude in degrees.
+    lat : float
+        Galactic latitude in degrees.
+    """
+    ra = np.radians(ra)
+    dec = np.radians(dec)
+    l = np.radians(33)
+    sinb = np.sin(dec) * np.sin(l) + np.cos(dec) * np.cos(l) * np.sin(ra - np.radians(282.25))
+    b = np.arcsin(sinb)
+    cosl = (np.sin(dec) - np.sin(b) * np.sin(l)) / (np.cos(b) * np.cos(l))
+    l = np.arccos(cosl)
+    lon = np.degrees(l) + 33
+    lat = np.degrees(b)
+    return lon, lat
+#从银道坐标转到 B1950.0 标准分点赤道坐标
+def galactic_to_equatorial(lon, lat):
+    """
+    Convert galactic coordinates to equatorial coordinates.
+
+    Parameters
+    ----------
+    lon : float
+        Galactic longitude in degrees.
+    lat : float
+        Galactic latitude in degrees.
+
+    Returns
+    -------
+    ra : float
+        Right ascension in degrees.
+    dec : float
+        Declination in degrees.
+    """
+    lon = np.radians(lon)
+    lat = np.radians(lat)
+    l = np.radians(33)
+    sindec = np.sin(lat) * np.sin(l) + np.cos(lat) * np.cos(l) * np.sin(lon - np.radians(33))
+    dec = np.arcsin(sindec)
+    cosra = (np.sin(lat) - np.sin(dec) * np.sin(l)) / (np.cos(dec) * np.cos(l))
+    ra = np.arccos(cosra) + np.radians(282.25)
+    ra = np.degrees(ra)
+    dec = np.degrees(dec)
+    return ra, dec
