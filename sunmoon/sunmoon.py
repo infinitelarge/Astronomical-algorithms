a54e3b7feb7cbb7ffbcd90b17da59db4a0d3f39c
infinitelarge <137892154+infinitelarge@users.noreply.github.com>
2023-07-29 21:47:26 +0800

    Added sunset and fixed sunrise algorithm err

--- sunmoon/sunmoon.py
+++ sunmoon/sunmoon.py
@@ -41,5 +41,36 @@ def sunrise(Jday,longitude,latitude):
     H = H * 24
     #Equation of time,from
     E = 4 * np.degrees((np.sin(M) - 0.0145 - np.sin(M_) * np.cos(F) + np.sin(2 * M_) * 0.0004 - np.cos(2 * M_) * 0.0004) * np.pi / 180)
-    H = H - E / 60
+    H = H + E / 60
+    return H
+#sunset
+def sunset(Jday,longitude,latitude):
+    D,M,M_,F,O = calc(Jday)
+    # 太阳视黄经
+    l = ct.sun_look_longtitude(Jday)
+    #太阳视赤经
+    alpha = ct.ecliptic_to_equatorial(ct.sun_true_longitude(Jday),ct.sun_true_latitude(Jday),Jday)[0]
+    #太阳视赤纬，公式来源：https://en.wikipedia.org/wiki/Sunrise_equation
+    delta = ct.ecliptic_to_equatorial(ct.sun_true_longitude(Jday),ct.sun_true_latitude(Jday),Jday)[1]
+    #太阳视地方时角，公式来源：https://en.wikipedia.org/wiki/Sunrise_equation
+    H = np.arccos((np.sin(np.radians(-0.83)) - np.sin(np.radians(latitude)) * np.sin(delta)) / (np.cos(np.radians(latitude)) * np.cos(delta)))
+    #太阳视地方时角
+    H = np.degrees(H)
+    #太阳视地方时角
+    H = H / 15
+    #太阳视地方时角
+    H = H + alpha
+    #太阳视地方时角
+    H = H - np.radians(longitude)
+    #太阳视地方时角
+    H = H / (2 * np.pi)
+    #太阳视地方时角
+    H = H - np.floor(H)
+    #太阳视地方时角，来自https://en.wikipedia.org/wiki/Sunrise_equation
+    H = H * 24
+    #Equation of time,来自https://en.wikipedia.org/wiki/Equation_of_time
+    E = 4 * np.degrees((np.sin(M) - 0.0145 - np.sin(M_) * np.cos(F) + np.sin(2 * M_) * 0.0004 - np.cos(2 * M_) * 0.0004) * np.pi / 180)
+    H = 24 - H
+    H = H + E/ 60
+     #日落时间
     return H
