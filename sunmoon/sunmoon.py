63508c82be1c53e54d6e4b92cc89c3c928a2beff
infinitelarge <137892154+infinitelarge@users.noreply.github.com>
2023-07-29 15:51:32 +0800

    Fixed an algorithm bug and used the new func

--- sunmoon/sunmoon.py
+++ sunmoon/sunmoon.py
@@ -20,9 +20,9 @@ def sunrise(Jday,longitude,latitude):
     # 太阳视黄经
     l = ct.sun_look_longtitude(Jday)
     #太阳视赤经
-    alpha = np.arctan2(np.cos(l) * np.sin(l), np.cos(l)) #O代表太阳视黄经，l代表太阳视黄纬
+    alpha = ct.ecliptic_to_equatorial(ct.sun_true_longitude(Jday),ct.sun_true_latitude(Jday),Jday)[0]
     #太阳视赤纬，公式来源：https://en.wikipedia.org/wiki/Sunrise_equation
-    delta = np.arcsin(np.sin(O) * np.sin(l))
+    delta = ct.ecliptic_to_equatorial(ct.sun_true_longitude(Jday),ct.sun_true_latitude(Jday),Jday)[1]
     #太阳视地方时角，公式来源：https://en.wikipedia.org/wiki/Sunrise_equation
     H = np.arccos((np.sin(np.radians(-0.83)) - np.sin(np.radians(latitude)) * np.sin(delta)) / (np.cos(np.radians(latitude)) * np.cos(delta)))
     #太阳视地方时角
